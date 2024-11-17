8dd0aa7c575dd47f64297ff4f15e0a42ac42e177
engchina <atjapan2015@gmail.com>
2024-11-17 08:22:30 +0800

    update

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from typing import List, Optional, Union, Dict, Any
 
 import numpy as np
 import tiktoken
+# from BCEmbedding import EmbeddingModel
+# from FlagEmbedding import BGEM3FlagModel
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 
 
+# Helper to serialize NumPy arrays
 def serialize_ndarray(value: Any) -> Any:
-    if isinstance(value, np.ndarray):
-        return value.tolist()
-    return value
+    return value.tolist() if isinstance(value, np.ndarray) else value
 
 
+# Pydantic models for request and response
 class EmbeddingsRequest(BaseModel):
     model: Optional[str] = None
     engine: Optional[str] = None
@@ -35,144 +37,102 @@ class EmbeddingsResponse(BaseModel):
     usage: UsageInfo
 
 
+# Constants and model initialization
 WORKER_API_EMBEDDING_BATCH_SIZE = int(os.getenv("FASTCHAT_WORKER_API_EMBEDDING_BATCH_SIZE", 4))
 
-# conan_embedding_v1 = EmbeddingModel(model_name_or_path="TencentBAC/Conan-embedding-v1", device='cuda',
-#                                     trust_remote_code=True)
-
-# xiaobu_embedding_v2 = EmbeddingModel(model_name_or_path="lier007/xiaobu-embedding-v2", device='cuda',
-#                                      trust_remote_code=True)
-
-#
-# bce_embedding_base_v1 = EmbeddingModel(model_name_or_path="maidalun1020/bce-embedding-base_v1", device='cuda',
-#                                        trust_remote_code=True)
-
-# bge_m3 = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True, device='cuda')
-
-# bge_multilingual_gemma2 = SentenceTransformer("BAAI/bge-multilingual-gemma2",
-#                                               model_kwargs={"torch_dtype": torch.float16})
-
-e5_large_instruct = SentenceTransformer('intfloat/multilingual-e5-large-instruct', trust_remote_code=True,
-                                        device='cuda')
-
-# e5_large = SentenceTransformer('intfloat/multilingual-e5-large', trust_remote_code=True,
-#                                device='cuda')
-
-# qte_qwen = SentenceTransformer("Alibaba-NLP/gte-Qwen1.5-7B-instruct", trust_remote_code=True, device='cuda')
-
-
-# zpoint_large_embedding_zh = SentenceTransformer('iampanda/zpoint_large_embedding_zh', device='cuda',
-#                                                 trust_remote_code=True)
+MODELS = {
+    "text-embedding-3-large": SentenceTransformer(
+        "intfloat/multilingual-e5-large-instruct", trust_remote_code=True, device="cuda"
+    ),
+    # "text-embedding-3-small": SentenceTransformer(
+    #     "intfloat/multilingual-e5-large-instruct", trust_remote_code=True, device="cuda"
+    # ),
+    # "text-embedding-ada-002": SentenceTransformer(
+    #     "intfloat/multilingual-e5-large-instruct", trust_remote_code=True, device="cuda"
+    # ),
+    # "multilingual-e5-large-instruct": SentenceTransformer(
+    #     "intfloat/multilingual-e5-large-instruct", trust_remote_code=True, device="cuda"
+    # ),
+    # "multilingual-e5-large": SentenceTransformer(
+    #     "intfloat/multilingual-e5-large", trust_remote_code=True, device="cuda"
+    # ),
+    # "Conan-embedding-v1": EmbeddingModel(
+    #     model_name_or_path="TencentBAC/Conan-embedding-v1", device="cuda", trust_remote_code=True
+    # ),
+    # "xiaobu-embedding-v2": EmbeddingModel(
+    #     model_name_or_path="lier007/xiaobu-embedding-v2", device="cuda", trust_remote_code=True
+    # ),
+    # "bce-embedding-base_v1": EmbeddingModel(
+    #     model_name_or_path="maidalun1020/bce-embedding-base_v1", device="cuda", trust_remote_code=True
+    # ),
+    # "bge-m3": BGEM3FlagModel("BAAI/bge-m3", use_fp16=True, device="cuda"),
+    # "bge-multilingual-gemma2": SentenceTransformer(
+    #     "BAAI/bge-multilingual-gemma2", model_kwargs={"torch_dtype": torch.float16}
+    # ),
+    # "gte-Qwen1.5-7B-instruct": SentenceTransformer(
+    #     "Alibaba-NLP/gte-Qwen1.5-7B-instruct", trust_remote_code=True, device="cuda"
+    # ),
+    # "zpoint_large_embedding_zh": SentenceTransformer(
+    #     "iampanda/zpoint_large_embedding_zh", device="cuda", trust_remote_code=True
+    # ),
+}
 
 app = FastAPI()
 
 
 def get_embedding_model(model_name: str):
-    print(f"{model_name=}")
-    # if model_name == 'text-embedding-3-small' or model_name == 'text-embedding-ada-002' or model_name == 'TencentBAC/Conan-embedding-v1' or model_name == 'TencentBAC/Conan-embedding-v1':
-    #     return conan_embedding_v1
-    # if model_name == 'text-embedding-3-small' or model_name == 'text-embedding-ada-002' or model_name == 'lier007/xiaobu-embedding-v2' or model_name == 'xiaobu-embedding-v2':
-    #     return xiaobu_embedding_v2
-    # elif model_name == 'lier007/xiaobu-embedding-v2' or model_name == 'xiaobu-embedding-v2':
-    #     return xiaobu_embedding_v2
-    # elif model_name == 'maidalun1020/bce-embedding-base_v1' or model_name == 'bce-embedding-base_v1':
-    #     return bce_embedding_base_v1
-    # elif model_name == 'iampanda/zpoint_large_embedding_zh' or model_name == 'zpoint_large_embedding_zh':
-    #     return zpoint_large_embedding_zh
-    # if model_name == 'BAAI/bge-m3' or model_name == 'bge-m3':
-    #     return bge_m3
-    # elif model_name == 'BAAI/bge-multilingual-gemma2' or model_name == 'bge-multilingual-gemma2':
-    #     return bge_multilingual_gemma2
-    if model_name == 'text-embedding-3-large' or model_name == 'text-embedding-3-small' or model_name == 'text-embedding-ada-002' or model_name == 'multilingual-e5-large-instruct':
-        return e5_large_instruct
-    # if model_name == 'intfloat/multilingual-e5-large' or model_name == 'multilingual-e5-large':
-    #     return e5_large
-    # elif model_name == 'Alibaba-NLP/gte-Qwen1.5-7B-instruct':
-    #     return qte_qwen
-    else:
+    """Fetch the embedding model by name."""
+    model = MODELS.get(model_name)
+    if not model:
         raise ValueError(f"Unsupported model: {model_name}")
+    return model
 
 
-def process_input(model_name, inp):
+def process_input(model_name: str, inp: Union[str, List[Any]]):
+    """Preprocess the input data."""
     if isinstance(inp, str):
-        inp = [inp]
-    elif isinstance(inp, list):
-        if isinstance(inp[0], int):
-            try:
-                decoding = tiktoken.model.encoding_for_model(model_name)
-            except KeyError:
-                model = "cl100k_base"
-                decoding = tiktoken.get_encoding(model)
-            inp = [decoding.decode(inp)]
-        elif isinstance(inp[0], list):
-            try:
-                decoding = tiktoken.model.encoding_for_model(model_name)
-            except KeyError:
-                model = "cl100k_base"
-                decoding = tiktoken.get_encoding(model)
-            inp = [decoding.decode(text) for text in inp]
+        return [inp]
+    if isinstance(inp, list):
+        try:
+            encoding = tiktoken.model.encoding_for_model(model_name)
+        except KeyError:
+            encoding = tiktoken.get_encoding("cl100k_base")
+        return [encoding.decode(item) if isinstance(item, (list, int)) else item for item in inp]
     return inp
 
 
 @app.post("/v1/embeddings")
 @app.post("/v1/engines/{model_name}/embeddings")
 async def create_embeddings(request: EmbeddingsRequest, model_name: str = None) -> dict:
-    print(f"{request.model=}")
-    print(f"{model_name=}")
-    if request.model is None:
-        request.model = model_name
+    """Endpoint to generate embeddings."""
+    request.model = request.model or model_name
     try:
         embedding_model = get_embedding_model(request.model)
-        print(f"{embedding_model=}")
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-    request.input = process_input(request.model, request.input)
-    data = []
-    token_num = 0
+    inputs = process_input(request.model, request.input)
+    data, token_num = [], 0
     batch_size = WORKER_API_EMBEDDING_BATCH_SIZE
-    batches = [
-        request.input[i: min(i + batch_size, len(request.input))]
-        for i in range(0, len(request.input), batch_size)
-    ]
 
-    for num_batch, batch in enumerate(batches):
-        if request.model == 'BAAI/bge-m3' or request.model == 'bge-m3':
-            embeddings = [embedding_model.encode(sentence)['dense_vecs'] for sentence in batch]
-        elif request.model == 'BAAI/bge-multilingual-gemma2' or request.model == 'bge-multilingual-gemma2':
-            for sentence in batch:
-                print(f"{sentence=}")
-            embeddings = [embedding_model.encode(sentence) for sentence in batch]
-        elif request.model == 'lier007/xiaobu-embedding-v2' or request.model == 'xiaobu-embedding-v2':
-            embeddings = embedding_model.encode(batch, normalize_embeddings=True)
-        elif request.model == 'text-embedding-3-small' or request.model == 'text-embedding-ada-002' or request.model == 'xiaobu-embedding-v2':
-            embeddings = embedding_model.encode(batch, normalize_embeddings=True)
-        else:
-            embeddings = embedding_model.encode(batch)
-        data += [
+    # Process inputs in batches
+    for num_batch, batch in enumerate(
+            [inputs[i: i + batch_size] for i in range(0, len(inputs), batch_size)]
+    ):
+        embeddings = embedding_model.encode(batch)
+        data.extend(
             {
                 "object": "embedding",
                 "embedding": serialize_ndarray(emb),
                 "index": num_batch * batch_size + i,
             }
             for i, emb in enumerate(embeddings)
-        ]
+        )
         token_num += sum(len(sent) for sent in batch)
 
-    usage = UsageInfo(
-        prompt_tokens=token_num,
-        total_tokens=token_num,
-        completion_tokens=None,
-    )
-
-    response = EmbeddingsResponse(
-        data=data,
-        model=request.model,
-        usage=usage,
-    )
-
-    response_dump = response.model_dump(exclude_none=True)
-    return response_dump
+    usage = UsageInfo(prompt_tokens=token_num, total_tokens=token_num)
+    response = EmbeddingsResponse(data=data, model=request.model, usage=usage)
+    return response.model_dump(exclude_none=True)
 
 
 if __name__ == "__main__":
